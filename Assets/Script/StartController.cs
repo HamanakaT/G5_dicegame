de6f53ff4e64a2727988923bb2e920f14e2ae32d
HamanakaT <154488836+HamanakaT@users.noreply.github.com>
2024-01-09 19:54:41 +0900

    タイトルの作成

--- Assets/Script/StartController.cs
+++ Assets/Script/StartController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
     }
 }
